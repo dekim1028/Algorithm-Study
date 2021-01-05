e65ce5ce5457174316bd12c6a824c0b9e5e515b1
dekim1028 <daeun235194@gmail.com>
2021-01-05 12:17:47 +0900

    다익스트라 예제

--- CodingTest_Example/Dijkstra/EX1_다익스트라 예제.js
+++ CodingTest_Example/Dijkstra/EX1_다익스트라 예제.js
@@ -4,11 +4,63 @@
  */
 
 function solution() {
+  const N = 6;
+  const M = 11;
+  const start = 1;
+  const graph = [
+    [],
+    [
+      [2, 2],
+      [3, 5],
+      [4, 1],
+    ],
+    [
+      [3, 3],
+      [4, 2],
+    ],
+    [
+      [2, 3],
+      [6, 5],
+    ],
+    [
+      [3, 3],
+      [5, 1],
+    ],
+    [
+      [3, 1],
+      [6, 2],
+    ],
+  ];
+
+  const distance = Array.from({ length: N + 1 }, () => Infinity);
+
+  //1. 시작 인덱스 queue 삽입
   const priorityQueue = new PriorityQueue();
+  priorityQueue.enqueue([start, 0]);
 
-  priorityQueue.enqueue([1, 2, 22]);
-  priorityQueue.enqueue([2, 3, 5]);
-  priorityQueue.enqueue([3, 4, 10]);
+  //2. 시작 인덱스 거리 0
+  distance[start] = 0;
 
-  console.log(priorityQueue.dequeue()); // Student('Pengsoo', 10)
+  while (priorityQueue.store.length > 0) {
+    const data = priorityQueue.dequeue();
+    const now = data[0];
+    const dist = data[1];
+
+    //3. 방문한적 있으면 무시
+    if (distance[now] < dist) continue;
+
+    if (!graph[now]) break;
+
+    //4. 현재 노드에서 인접한 노드까지의 거리 체크
+    graph[now].forEach((target) => {
+      const cost = dist + target[1];
+
+      if (cost < distance[target[0]]) {
+        distance[target[0]] = cost;
+        priorityQueue.enqueue([target[0], cost]);
+      }
+    });
+  }
+
+  return distance.splice(1);
 }
--- CodingTest_Example/Dijkstra/PriorityQueue.js
+++ CodingTest_Example/Dijkstra/PriorityQueue.js
@@ -10,10 +10,11 @@ class PriorityQueue {
   dequeue() {
     let entry = 0;
     this.store.forEach((item, index) => {
-      if (this.store[entry][2] < this.store[index][2]) {
+      if (this.store[entry][1] > this.store[index][1]) {
         entry = index;
       }
     });
-    return this.store.splice(entry, 1);
+
+    return this.store.splice(entry, 1)[0];
   }
 }
